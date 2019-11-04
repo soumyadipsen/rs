49e39e562d96bc5bb59a74fa163eae23fcc80343
SohamSarkar5 <sohamsarkar.17@gmail.com>
2019-11-04 15:01:43 +0530

    Update rc.js

--- rc.js
+++ rc.js
@@ -1,14 +1,115 @@
-var s1_ingredient=["Chicken","Salt","Red Chili Powder","Garam Masala","Yogurt","Butter","Onion","Sugar","Cashew Nuts"];
-var s1_pos=["RA: S1","RA: S2","RA: S3","RA: S4","RA: S5","RB: S1","RB: S2","RB: S3","RB: S4"];
-var s1_price=[150,120,50,50,100,100,80,60,120];
+var s1_rows=[
+        {
+            Ingredient:"Chicken"
+            Pos:"RA: S1"
+            Price:150
+        },
+        {
+            Ingredient:"Salt"
+            Pos:"RA: S2"
+            Price:120
+        } ,
+        {
+            Ingredient:"Red Chili Powder"
+            Pos:"RA: S3"
+            Price:50
+        } ,
+        {
+            Ingredient:"Garam Masala"
+            Pos:"RA: S4"
+            Price:50
+        } ,
+        {
+            Ingredient:"Yogurt"
+            Pos:"RA: S5"
+            Price:100
+        } ,
+        {
+            Ingredient:"Butter"
+            Pos:"RB: S1"
+            Price:50
+        } ,
+        {
+            Ingredient:"Cashew Nuts"
+            Pos:"RB: S2"
+            Price:200
+        }
+    ]
 
-var s2_ingredient=["Paneer","Salt","Red Chili Powder","Garam Masala","Yogurt","Vegetable Oil","Onion","Sugar","Cashew Nuts"];
-var s2_pos=["RC: S1","RA: S2","RA: S3","RA: S4","RA: S5","RB: S5","RB: S2","RB: S3","RB: S4"];
-var s2_price=[150,120,50,50,100,200,80,60,120];
 
+    var s2_rows=[
+        {
+            Ingredient:"Paneer"
+            Pos:"RB: S3"
+            Price:150
+        },
+        {
+            Ingredient:"Salt"
+            Pos:"RA: S2"
+            Price:120
+        } ,
+        {
+            Ingredient:"Red Chili Powder"
+            Pos:"RA: S3"
+            Price:50
+        } ,
+        {
+            Ingredient:"Garam Masala"
+            Pos:"RA: S4"
+            Price:50
+        } ,
+        {
+            Ingredient:"Yogurt"
+            Pos:"RA: S5"
+            Price:100
+        } ,
+        {
+            Ingredient:"Butter"
+            Pos:"RB: S1"
+            Price:50
+        } ,
+        {
+            Ingredient:"Cashew Nuts"
+            Pos:"RB: S2"
+            Price:200
+        }
+    ]
 
-var s3_ingredient=["Chicken","Salt","Lemon Juice","Cinnamon","Yogurt","Vegetable Oil","Ginger","Sugar","Heavy Cream"];
-var s3_pos=["RA: S1","RA: S2","RC: S1","RC: S2","RA: S5","RB: S5","RC: S3","RB: S3","RC: S4"];
-var s3_price=[150,120,50,50,100,200,70,60,60];
-
+    var s3_rows=[
+        {
+            Ingredient:"Chicken"
+            Pos:"RA: S1"
+            Price:150
+        },
+        {
+            Ingredient:"Salt"
+            Pos:"RA: S2"
+            Price:120
+        } ,
+        {
+            Ingredient:"Lemon Juice"
+            Pos:"RB: S3"
+            Price:50
+        } ,
+        {
+            Ingredient:"Garam Masala"
+            Pos:"RA: S4"
+            Price:50
+        } ,
+        {
+            Ingredient:"Yogurt"
+            Pos:"RA: S5"
+            Price:100
+        } ,
+        {
+            Ingredient:"Butter"
+            Pos:"RB: S1"
+            Price:50
+        } ,
+        {
+            Ingredient:"Heavy Cream"
+            Pos:"RB: S4"
+            Price:60
+        } 
+    ]
 
