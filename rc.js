db20c5d5e5031594130f8efea2751f658e9bed0c
SohamSarkar5 <sohamsarkar.17@gmail.com>
2019-11-04 14:13:39 +0530

    Recommendation Database

---
+++ rc.js
@@ -0,0 +1,14 @@
+var s1_ingredient=["Chicken","Salt","Red Chili Powder","Garam Masala","Yogurt","Butter","Onion","Sugar","Cashew Nuts"];
+var s1_pos=["RA: S1","RA: S2","RA: S3","RA: S4","RA: S5","RB: S1","RB: S2","RB: S3","RB: S4"];
+var s1_price=[150,120,50,50,100,100,80,60,120];
+
+var s2_ingredient=["Paneer","Salt","Red Chili Powder","Garam Masala","Yogurt","Vegetable Oil","Onion","Sugar","Cashew Nuts"];
+var s2_pos=["RC: S1","RA: S2","RA: S3","RA: S4","RA: S5","RB: S5","RB: S2","RB: S3","RB: S4"];
+var s2_price=[150,120,50,50,100,200,80,60,120];
+
+
+var s3_ingredient=["Chicken","Salt","Lemon Juice","Cinnamon","Yogurt","Vegetable Oil","Ginger","Sugar","Heavy Cream"];
+var s3_pos=["RA: S1","RA: S2","RC: S1","RC: S2","RA: S5","RB: S5","RC: S3","RB: S3","RC: S4"];
+var s3_price=[150,120,50,50,100,200,70,60,60];
+
+
