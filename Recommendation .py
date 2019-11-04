70de64b8445c379371f8033745c4bbaaa14b2aff
Soumyadip Sen <soumya.sen.98@gmail.com>
2019-11-04 14:02:45 +0530

    Add files via upload

---
+++ Recommendation .py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtPrintSupport import *
+
+import os
+import sys
+
+
+class AboutDialog(QDialog):
+    def __init__(self, *args, **kwargs):
+        super(AboutDialog, self).__init__(*args, **kwargs)
+
+        QBtn = QDialogButtonBox.Ok  # No cancel
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        layout = QVBoxLayout()
+
+        title = QLabel("Recommendation System")
+        font = title.font()
+        font.setPointSize(20)
+        title.setFont(font)
+
+        layout.addWidget(title)
+
+        logo = QLabel()
+        
+
+       
+
+        for i in range(0, layout.count()):
+            layout.itemAt(i).setAlignment(Qt.AlignHCenter)
+
+        layout.addWidget(self.buttonBox)
+
+        self.setLayout(layout)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl("http://localhost/cms"))
+
+        self.browser.urlChanged.connect(self.update_urlbar)
+        self.browser.loadFinished.connect(self.update_title)
+        self.setCentralWidget(self.browser)
+
+        self.status = QStatusBar()
+        self.setStatusBar(self.status)
+        self.show()
+
+        self.setWindowIcon(QIcon(os.path.join('images', 'ma-icon-64.png')))
+
+    def update_title(self):
+        title = self.browser.page().title()
+        self.setWindowTitle("%s" % title)
+
+    def navigate_mozarella(self):
+        self.browser.setUrl(QUrl("https://www.udemy.com/522076"))
+
+    def about(self):
+        dlg = AboutDialog()
+        dlg.exec_()
+
+    def open_file(self):
+        filename, _ = QFileDialog.getOpenFileName(self, "Open file", "",
+                                                  "Hypertext Markup Language (*.htm *.html);;"
+                                                  "All files (*.*)")
+
+        if filename:
+            with open(filename, 'r') as f:
+                html = f.read()
+
+            self.browser.setHtml(html)
+            self.urlbar.setText(filename)
+
+    def save_file(self):
+        filename, _ = QFileDialog.getSaveFileName(self, "Save Page As", "",
+                                                  "Hypertext Markup Language (*.htm *html);;"
+                                                  "All files (*.*)")
+
+        if filename:
+            html = self.browser.page().toHtml()
+            with open(filename, 'w') as f:
+                f.write(html)
+
+    def print_page(self):
+        dlg = QPrintPreviewDialog()
+        dlg.paintRequested.connect(self.browser.print_)
+        dlg.exec_()
+
+    def navigate_home(self):
+        self.browser.setUrl(QUrl("http://www.google.com"))
+
+    def navigate_to_url(self):  # Does not receive the Url
+        q = QUrl(self.urlbar.text())
+        if q.scheme() == "":
+            q.setScheme("http")
+
+        self.browser.setUrl(q)
+
+    def update_urlbar(self, q):
+
+        if q.scheme() == 'https':
+            # Secure padlock icon
+            self.httpsicon.setPixmap(QPixmap(os.path.join('images', 'lock-ssl.png')))
+
+        else:
+            # Insecure padlock icon
+            self.httpsicon.setPixmap(QPixmap(os.path.join('images', 'lock-nossl.png')))
+
+        self.urlbar.setText(q.toString())
+        self.urlbar.setCursorPosition(0)
+
+
+app = QApplication(sys.argv)
+app.setApplicationName("Recommendation System")
+
+window = MainWindow()
+
+app.exec_()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
